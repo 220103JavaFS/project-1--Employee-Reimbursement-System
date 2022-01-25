97b51de440fd72c09b6ab7c2424bd86ffebe237a
ivovlad <ivo000@gbg.bg>
2022-01-25 14:53:19 -0500

    Ivo deleted class Trial and comments in Controller

--- src/main/java/com/revature/controllers/Controller.java
+++ src/main/java/com/revature/controllers/Controller.java
@@ -5,9 +5,8 @@ import io.javalin.Javalin;
 
 public interface Controller { void addRoutes(Javalin app);}
 
-//Ivo's comment
+
 //Zach's comment
 //Conflict was resolved
 
 //Zach's change in the side branch
-//Comment in main branch by Ivo

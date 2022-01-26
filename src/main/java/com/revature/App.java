d4f63ebd1b7f84a55e28dcd84ac72a4125f10d66
ivovlad <ivo000@gbg.bg>
2022-01-26 15:36:01 -0500

    Testing of RequestService

--- src/main/java/com/revature/App.java
+++ src/main/java/com/revature/App.java
@@ -11,7 +11,7 @@ public class App {
 
     public static void main(String[] args) {
         app = Javalin.create((config)->{
-            config.addStaticFiles("C:\\Users\\zbyrs\\Desktop\\Project1\\HelloProject1.js",
+            config.addStaticFiles("C:\\Users\\ivo00\\Documents\\revature\\project-1--zachivo\\Project1 - Webpage\\HelloProject1.js",
                     Location.EXTERNAL);
         });
         configure(new LoginController());

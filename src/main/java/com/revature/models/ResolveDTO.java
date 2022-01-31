a1e77c27fc5a02ec4711dbd35cf06c30a102621f
ivovlad <ivo000@gbg.bg>
2022-01-30 23:04:42 -0500

    Resolve request functionality

---
+++ src/main/java/com/revature/models/ResolveDTO.java
@@ -0,0 +1,24 @@
+package com.revature.models;
+
+public class ResolveDTO {
+    public String resolveChoice;
+    public int requestID;
+    public int authorId;
+    public String dateResolved;
+
+    public ResolveDTO(String resolveChoice, int requestID, int authorId, String dateResolved) {
+        this.resolveChoice = resolveChoice;
+        this.requestID = requestID;
+        this.authorId = authorId;
+        this.dateResolved = dateResolved;
+    }
+
+    public ResolveDTO() {
+    }
+
+    public ResolveDTO(String resolveChoice, int requestID) {
+        this.resolveChoice = resolveChoice;
+        this.requestID = requestID;
+
+    }
+}
